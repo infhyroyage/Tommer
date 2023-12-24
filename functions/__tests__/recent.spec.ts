0a5be0a7aaf48fbdc7f110aad60e1be77c94e502
Takeru KIDO <keidy2463@gmail.com>
2023-12-25 00:37:27 +0900

    Update validation test

--- functions/__tests__/recent.spec.ts
+++ functions/__tests__/recent.spec.ts
@@ -36,16 +36,128 @@ describe("[PUT] /recent", () => {
   //   });
   // });
 
-  it("Should return 400 if the type of request body is number", async () => {
-    const requestBody: number = 0;
-    const response = await recent(
-      new HttpRequest({
-        ...httpRequestInitDefault,
-        body: { string: JSON.stringify(requestBody) },
-      }),
-      new InvocationContext({})
-    );
+  describe("Validation", () => {
+    it("Should return 400 if the type of request body is not object", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: { string: JSON.stringify(0) },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
 
-    expect(response.status).toBe(400);
+    it("Should return 400 if there is a invalid attribute of request body", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: { string: JSON.stringify({ dummy: "dummy" }) },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
+
+    it("Should return 400 if the type of each makers is not string", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: { string: JSON.stringify({ makers: [0], prev: [] }) },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
+
+    it("Should return 400 if the type of previous ucs is not object", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: { string: JSON.stringify({ makers: [], prev: [0] }) },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
+
+    it("Should return 400 if there is a invalid attribute of previous ucs", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: {
+            string: JSON.stringify({ makers: [], prev: [{ dummy: "dummy" }] }),
+          },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
+
+    it("Should return 400 if the type of previous ucs maker is not string", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: {
+            string: JSON.stringify({
+              makers: [],
+              prev: [{ maker: 0, no: 1, name: "name", upload: "upload" }],
+            }),
+          },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
+
+    it("Should return 400 if the type of previous ucs no is not number", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: {
+            string: JSON.stringify({
+              makers: [],
+              prev: [
+                { maker: "maker", no: "no", name: "name", upload: "upload" },
+              ],
+            }),
+          },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
+
+    it("Should return 400 if the type of previous ucs name is not string", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: {
+            string: JSON.stringify({
+              makers: [],
+              prev: [{ maker: "maker", no: 1, name: 0, upload: "upload" }],
+            }),
+          },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
+
+    it("Should return 400 if the type of previous ucs upload is not string", async () => {
+      const response = await recent(
+        new HttpRequest({
+          ...httpRequestInitDefault,
+          body: {
+            string: JSON.stringify({
+              makers: [],
+              prev: [{ maker: "maker", no: 1, name: "name", upload: 0 }],
+            }),
+          },
+        }),
+        new InvocationContext({})
+      );
+      expect(response.status).toBe(400);
+    });
   });
 });
